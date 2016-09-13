0b0408a547accecaf6a353209ace2850d0d29096
Clayton Lantz <klayton1579@gmail.com>
2016-09-13 15:12:48 -0600

    added the folder to hold the examples from the back end prep section

---
+++ back_end_prep/Introduction to programming examples/basics_exercises.rb
@@ -0,0 +1,50 @@
+#basics exercises
+
+#1:
+
+"Clayton " + "Lantz"
+puts ""
+
+#2:
+
+thousands = 1579 / 1000
+hundreds = 1579 % 1000 / 100
+tens = 1579 % 1000 % 100 / 10
+ones = 1579 % 1000 %100 % 10
+puts thousands, hundreds, tens, ones
+puts ""
+
+#3:
+
+movies = {
+
+  south_park_bigger_longer_and_uncut: 1999,
+  the_good_the_bad_and_the_ugly: 1967,
+  the_internship: 2013
+}
+
+#4:
+
+dates = [1967, 1999, 2013]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts ""
+
+#5:
+
+puts 5*4*3*2*1
+puts 6*5*4*3*2*1
+puts 7*6*5*4*3*2*1
+puts 8*7*6*5*4*3*2*1
+puts ""
+
+#6:
+
+puts 1.1 * 1.1
+puts 2.2 * 2.2
+puts 3.3 * 3.3
+puts ""
+
+#7:Somebody missed an opening bracket somewhere
---
+++ back_end_prep/Introduction to programming examples/name.rb
@@ -0,0 +1,18 @@
+# name.rb
+puts "What is your name?"
+name = gets.chomp
+puts "Hello " + name
+
+#name.rb continued
+
+10.times do 
+  puts name
+end
+
+#name.rb continued once again
+
+puts "what is your first name?"
+first = gets.chomp
+puts "what is your last name?"
+last = gets.chomp
+puts first + " " + last
